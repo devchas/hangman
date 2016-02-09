9f986eaec6309c326211ff36f269445716d02987
S <s>
2016-02-09 17:46:03 -0500

    {"types":"C","runs":44}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -15,7 +15,7 @@ public class HangmanCanvas extends GCanvas {
 		y = (getHeight() - SCAFFOLD_HEIGHT) / 2	+ 200;
 		add(new GLine(x, y, x, y + SCAFFOLD_HEIGHT));
 		add(new GLine(x + BEAM_LENGTH, y, x, y));
-		GRect rect = new GRect(0, 0, getWidth(), 50);
+		GRect rect = new GRect(0, 0, 100, 50);
 		rect.setFilled(true);
 		add(rect);
 	}
