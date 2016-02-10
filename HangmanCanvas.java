9b2abd1195b8a628d063183cf16e3c8e615d22b4
S <s>
2016-02-09 19:58:23 -0500

    {"types":"C","runs":107}

--- HangmanCanvas.java
+++ HangmanCanvas.java
@@ -11,7 +11,7 @@ public class HangmanCanvas extends GCanvas {
 /** Resets the display so that only the scaffold appears */
 	public void reset() {
 		removeAll();
-		wrongGuesses = ""
+		wrongGuesses = "";
 		x = getWidth() / 2 - BEAM_LENGTH;
 		y = (getHeight() - SCAFFOLD_HEIGHT) / 2;
 		add(new GLine(x, y, x, y + SCAFFOLD_HEIGHT));
